5d866eb00d3aa10309d3aae3a5817f4e579a41e8
HansonLau <shadowguy368@gmail.com>
2021-03-21 15:17:49 -0700

    made font sise smaller on phone, tried correcting the shift when the nav bar connected to the top

--- scroll.js
+++ scroll.js
@@ -1,14 +1,17 @@
 window.onscroll = function() {myFunction()};
 
     var nav = document.getElementById("nav");
+    var des = document.getElementById("de");
 
     var sticky = nav.offsetTop;
 
     function myFunction() {
         if (window.pageYOffset >= sticky) {
-            nav.classList.add("sticky")
+            nav.classList.add("sticky");
+            des.classList.add("correction");
         } 
         else {
             nav.classList.remove("sticky");
+            des.classList.remove("correction");
         }
     }
