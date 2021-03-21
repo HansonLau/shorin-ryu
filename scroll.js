44a55ddfa3e7e2dd3a12335cbe5699491419310c
HansonLau <shadowguy368@gmail.com>
2021-03-21 15:29:33 -0700

    added more padding between footer and main body to prevent screen shaking

--- scroll.js
+++ scroll.js
@@ -1,17 +1,14 @@
 window.onscroll = function() {myFunction()};
 
     var nav = document.getElementById("nav");
-    var des = document.getElementById("de");
 
     var sticky = nav.offsetTop;
 
     function myFunction() {
         if (window.pageYOffset >= sticky) {
             nav.classList.add("sticky");
-            des.classList.add("correction");
         } 
         else {
             nav.classList.remove("sticky");
-            des.classList.remove("correction");
         }
     }
